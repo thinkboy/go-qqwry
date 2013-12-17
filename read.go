9ca80c10dd9bba5dca42717ee8f40e4e215434b4
thinkboy <liuding_234@yahoo.com.cn>
2013-12-17 16:40:36 +0800

    Update read.go

--- read.go
+++ read.go
@@ -32,14 +32,3 @@ func read4byte(file helper, offset int64) (ret int64) {
 	ret = int64(buffer[0]) | int64(buffer[1])<<8 | int64(buffer[2])<<16 | int64(buffer[3])<<24
 	return
 }
-
-/*
-func readbytes(ip net.IP) int64 {
-	v := ip.To4()
-	return int64(v[0])<<24 | int64(v[1])<<16 | int64(v[2])<<8 | int64(v[3])
-}
-
-func readslice(v int64) net.IP {
-	return net.IPv4(byte((v&0xFF000000)>>24), byte((v&0xFF0000)>>16), byte((v&0xFF00)>>8), byte(v&0xFF))
-}
-*/
